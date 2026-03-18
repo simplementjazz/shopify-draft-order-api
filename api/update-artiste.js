8674fee7fdf37880ce69894c3b18b1734ff70fe5
simplementjazz <robert@simplementjazz.com>
2026-03-18 16:30:55 -0400

    Add CORS support for OPTIONS method

--- api/update-artiste.js
+++ api/update-artiste.js
@@ -3,6 +3,7 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
