5cb45aca1fae69112842b561014c6af7e501ec5c
simplementjazz <robert@simplementjazz.com>
2026-03-31 11:50:31 -0400

    Rename file reference in inscription-artiste.js

--- api/inscription-artiste.js
+++ api/inscription-artiste.js
@@ -1,4 +1,4 @@
-// api/update-artiste.js
+// api/inscription-artiste.js
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'https://www.paiementmusique.ca');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
